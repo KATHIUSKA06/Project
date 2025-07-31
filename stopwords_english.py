93831f2742c2a3f79eea5ee15270fc2a0e61f26c
KATHIUSKA06 <kathiuska06@hotmail.com>
2025-07-30 20:43:02 -0500

    Exploramos las stopwords en ingles

--- stopwords_english.py
+++ stopwords_english.py
@@ -3,9 +3,6 @@ import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 
-lista_stopwords = stopwords.words('spanish')
-
 lista_stopwords = stopwords.words('english')
 
 print(lista_stopwords)
-    
